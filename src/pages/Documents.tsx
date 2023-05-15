cd7f819a820f0a4187f4c7e165e69b0c292db1e7
Bryndin Alexander <alexanderbrendin@gmail.com>
2023-05-15 20:11:34 +0300

    Fixed Scrolling

--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,29 +1,65 @@
-import { useState } from "react";
-import DocumentList from "../components/DocumentList/DocumentList";
-import styles from "./Documents.module.scss";
-import { FileData } from "../models/fileData";
+import { useState } from 'react';
+import DocumentList from '../components/DocumentList/DocumentList';
+import styles from './Documents.module.scss'
+import { FileData } from '../models/fileData';
 
 const Documents = () => {
-  const [data] = useState<FileData[]>([
+  const [data, setData] = useState<FileData[]>([
     {
       id: 1,
-      fileName: "Rundofase",
-      lastEdit: "08/08/2020",
-      file: "",
+      fileName: 'Rundofase',
+      lastEdit: '08/08/2020',
+      file: '',
     },
     {
       id: 2,
-      fileName: "Genco Pura Olive Oil Company",
-      lastEdit: "09/08/2020",
-      file: "",
+      fileName: 'Genco Pura Olive Oil Company',
+      lastEdit: '09/08/2020',
+      file: '',
     },
     {
       id: 3,
-      fileName: "Bubba Gump",
-      lastEdit: "10/08/2020",
-      file: "",
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
     },
-  ]);
+    {
+      id: 4,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+    {
+      id: 5,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+    {
+      id: 6,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+    {
+      id: 7,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+    {
+      id: 8,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+    {
+      id: 9,
+      fileName: 'Bubba Gump',
+      lastEdit: '10/08/2020',
+      file: '',
+    },
+  ])
   return (
     <div className={styles.documents}>
       <DocumentList dataSource={data} />
